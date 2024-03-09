03c75ca2ef8b3f9da69a6ace7900c0330de83c8d
Pumbkinpat <Ptuan0898@gmail.com>
2024-03-09 14:22:57 +0700

    Change folders name

---
+++ Test/Test.js
@@ -0,0 +1,8 @@
+// This is optional, but you can use JavaScript to enhance your navigation bar.
+
+// For example, you could add a toggle for mobile view:
+
+function toggleNav() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('responsive');
+}
